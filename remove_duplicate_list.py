74531826286e28698d0a594b96854cbca0a7073e
sai1591 <65250547+sai1591@users.noreply.github.com>
2020-10-09 09:55:42 +0530

    Create remove_duplicate_list.py

---
+++ remove_duplicate_list.py
@@ -0,0 +1,7 @@
+test_list = [1, 3, 5, 6, 3, 5, 6, 1] 
+print ("The original list is : " +  str(test_list)) 
+res = [] 
+for i in test_list: 
+    if i not in res: 
+        res.append(i)  
+print ("The list after removing duplicates : " + str(res)) 
